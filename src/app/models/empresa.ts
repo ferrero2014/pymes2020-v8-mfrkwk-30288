a0ad916facd8efe13205d6e98b0f3a6ff0086241
ferrero2014 <ferrerocarina@gmail.com>
2020-11-29 08:58:52 -0300

    1 file added

---
+++ src/app/models/empresa.ts
@@ -0,0 +1,13 @@
+export class Empresa {
+  IdArticulo: number;
+  Nombre: string;
+  FechaFundacion: string;
+  CantidadEnpleados: number;
+  };
+export const Empresas: Empresa[] = [
+  {
+    IdArticulo: 1,
+    Nombre: "ARCOR",
+    FechaFundacion:20/08/2000,
+    CantidadEnpleados:30
+  },
